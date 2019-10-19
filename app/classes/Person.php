9d6b6f249636b3a581af5e85e16ebacde8e7324a
Neel Bhanushali <neal.bhanushali@relifems.com>
2019-10-19 14:20:45 +0530

    class, setter, getters done

--- app/classes/Person.php
+++ app/classes/Person.php
@@ -2,10 +2,43 @@
 
 namespace App\Classes;
 
+use Carbon\Carbon;
+use Exception;
+
 class Person
 {
+    private $name, $dob;
+
     public function __construct()
     {
-        echo 'Person created.';
+        // 
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name = 'John Doe')
+    {
+        $this->name = $name;
+    }
+
+    public function getDob()
+    {
+        return $this->dob;
+    }
+
+    public function setDob($dob)
+    {
+        if (Carbon::createFromFormat('Y-m-d', $dob) === false) {
+            throw new Exception('Date should be in format Y-m-d');
+        }
+        $this->dob = $dob;
+    }
+
+    public function age()
+    {
+        return Carbon::now()->diff($this->getDob())->format("%Y");
     }
 }
--- index.php
+++ index.php
@@ -5,3 +5,30 @@ use App\Classes\Person;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $person = new Person;
+$person->setName('Scarlett Johansson');
+$person->setDob('1984-11-22');
+
+
+?>
+
+<details open style="display: inline-block">
+    <summary>Object</summary>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>DOB</th>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <td><?php echo $person->getName(); ?></td>
+            <td><?php echo $person->getDob(); ?></td>
+            <td><?php echo $person->age(); ?></td>
+        </tr>
+    </table>
+</details>
+
+<style>
+    details {
+        display: inline-block;
+    }
+</style>
